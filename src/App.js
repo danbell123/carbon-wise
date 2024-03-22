412fc3a9cf1c45b770e0eb124522890ee367d8b4
danbell123 <d.bell13@newcastle.ac.uk>
2024-03-22 23:23:44 +0000

    created authorisation basics, private routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import DesktopMenu from '../src/components/menus/DesktopMenu';
 import MobileMenu from '../src/components/menus/MobileMenu';
 import Dashboard from '../src/pages/Dashboard/Dashboard';
 import LoginRegister from '../src/pages/LoginRegister';
+import { AuthProvider, useAuth } from './authContext'; // Adjust the import path as necessary
+
+// A component to protect routes
+function PrivateRoute({ children }) {
+  const { currentUser } = useAuth();
+  return currentUser ? children : <Navigate to="/" />;
+}
 
 const App = () => {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
@@ -22,17 +29,19 @@ const App = () => {
   const isMobile = windowWidth < 768; // Assume 768px is the breakpoint for mobile devices
 
   return (
-    <div className="App">
-      {isMobile ? <MobileMenu /> 
-                : <DesktopMenu />
-      }
-      <Router>
-        <Routes>
-          <Route path="/" element={<LoginRegister />} />
-          <Route path="/dashboard" element={<Dashboard />} />
-        </Routes>
-    </Router>
-    </div>
+    <AuthProvider>
+      <div className="App">
+        {isMobile ? <MobileMenu /> 
+                  : <DesktopMenu />
+        }
+        <Router>
+          <Routes>
+            <Route path="/" element={<LoginRegister />} />
+            <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+          </Routes>
+      </Router>
+      </div>
+    </AuthProvider>
   );
 };
 
---
+++ src/authContext.jsx
@@ -0,0 +1,31 @@
+import React, { useContext, useState, useEffect } from 'react';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+  return useContext(AuthContext);
+}
+
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const auth = getAuth();
+    const unsubscribe = onAuthStateChanged(auth, user => {
+      setCurrentUser(user);
+      setLoading(false);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  const value = { currentUser };
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  );
+};
