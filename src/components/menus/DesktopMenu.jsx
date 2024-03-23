3a1b7700bf8b11c1f97f3e80331544edb8ac7368
danbell123 <d.bell13@newcastle.ac.uk>
2024-03-23 22:20:19 +0000

    sm&lg menu styling

--- src/components/menus/DesktopMenu.jsx
+++ src/components/menus/DesktopMenu.jsx
@@ -5,18 +5,18 @@ import Logo from '../../assets/Logo.png';
 const DesktopMenu = () => {
   return (
     <aside className="h-screen fixed top-0 bg-bg-outer text-white w-64">
-      <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-col justify-between h-full p-5">
         <div>
-          <div className="p-5 pt-10">
+          <div className="pt-5 pb-10">
             <img src={Logo} alt="Carbon Wise Logo" className="w-full" />
           </div>
-          <nav className="flex flex-col px-5 space-y-3">
+          <nav className="flex flex-col space-y-3">
             <NavLink 
               to="/dashboard"
               className={({ isActive }) =>
                 isActive
-                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded flex items-center"
-                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded flex items-center"
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
               } 
             >
               <span className="material-symbols-outlined mr-1">home</span>
@@ -27,8 +27,8 @@ const DesktopMenu = () => {
               to="/carbon-intensity"
               className={({ isActive }) =>
                 isActive
-                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded flex items-center"
-                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded flex items-center"
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
               } 
             >
               <span className="material-symbols-outlined mr-1">monitoring</span>
@@ -40,45 +40,47 @@ const DesktopMenu = () => {
               to="/your-usage"
               className={({ isActive }) =>
                 isActive
-                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded flex items-center"
-                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded flex items-center"
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
               } 
             >
               <span className="material-symbols-outlined mr-1">electric_bolt</span>
               <p className='m-2'>Your Usage</p>
             </NavLink>
-
+            
             <NavLink 
               to="/your-scores"
               className={({ isActive }) =>
                 isActive
-                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded flex items-center"
-                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded flex items-center"
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
               } 
             >
               <span className="material-symbols-outlined mr-1">speed</span>
               <p className='m-2'>Your Scores</p>
             </NavLink>
-            
-
-
           </nav>
         </div>
-        <div className="p-5 bg-bg-main flex flex-col">
-
-          <div className="flex items-center space-x-3 text-base">
+        <div>
+        <div className="p-5 mb-5 bg-bg-main flex flex-col rounded-lg w-min">
+          <NavLink to="/your-scores" className="text-sm text-text-colour-secondary bg-transparent hover:bg-bg-main rounded-lg flex items-center">
             <span className="material-symbols-outlined">
-              account_circle
+              dark_mode
             </span>
-            <span>Dan Bell</span>
-          </div>
-
-          <NavLink to="/your-scores" className="text-sm text-text-colour-secondary bg-transparent hover:bg-bg-main rounded flex items-center">
-            <p>Settings</p>
           </NavLink>
-
+          </div>
+          <div className="p-5 mb-10 bg-bg-main flex flex-col rounded-lg">
+            <div className="flex items-center space-x-3 text-base">
+              <span className="material-symbols-outlined">
+                account_circle
+              </span>
+              <span>Dan Bell</span>
+            </div>
+            <NavLink to="/your-scores" className="text-sm text-text-colour-secondary bg-transparent hover:bg-bg-main rounded-lg flex items-center">
+              <p className='mb-0'>Settings</p>
+            </NavLink>
+          </div>
         </div>
-
       </div>
     </aside>
   );
--- src/components/menus/MobileMenu.jsx
+++ src/components/menus/MobileMenu.jsx
@@ -1,6 +1,7 @@
 // MobileMenu.jsx
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import Logo from '../../assets/Logo.png';
 
 const MobileMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -46,36 +47,75 @@ const MobileMenu = () => {
 
       {/* Menu Items */}
       <aside
-        className={`transform top-0 left-0 w-64 bg-gray-800 text-white fixed h-full overflow-auto z-30 ease-in-out transition-all duration-300 ${
+        className={`transform top-0 left-0 w-64 bg-bg-outer fixed h-full overflow-auto z-30 ease-in-out transition-all duration-300 ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
         {/* Menu header */}
-        <button
-            className="text-white focus:outline-none bg-transparent p-5"
+        <div className='flex flex-row'>
+          <button
+            className="text-white focus:outline-none bg-transparent pl-5"
             onClick={() => setIsOpen(!isOpen)}
           >
             <span className="material-symbols-outlined">close</span>
           </button>
-        <div className="text-xl font-bold p-5 bg-green-500">
-          CarbonWISE
+
+          <div className="p-5">
+            <img src={Logo} alt="Carbon Wise Logo" className="w-full" />
+          </div>
         </div>
 
         {/* Navigation Links */}
-        <nav className="flex flex-col p-5">
-          <NavLink to="/dashboard" className="p-2 hover:bg-green-600 rounded">
-            Dashboard
-          </NavLink>
-          <NavLink to="/carbon-intensity" className="p-2 hover:bg-green-600 rounded">
-            Carbon Intensity
-          </NavLink>
-          <NavLink to="/your-usage" className="p-2 hover:bg-green-600 rounded">
-            Your Usage
-          </NavLink>
-          <NavLink to="/your-scores" className="p-2 hover:bg-green-600 rounded">
-            Your Scores
-          </NavLink>
-        </nav>
+        <nav className="flex flex-col space-y-3 p-5">
+            <NavLink 
+              to="/dashboard"
+              className={({ isActive }) =>
+                isActive
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
+              } 
+            >
+              <span className="material-symbols-outlined mr-1">home</span>
+              <p className='m-2'>Dashboard</p>
+            </NavLink>
+
+            <NavLink 
+              to="/carbon-intensity"
+              className={({ isActive }) =>
+                isActive
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
+              } 
+            >
+              <span className="material-symbols-outlined mr-1">monitoring</span>
+              <p className='m-2'>Carbon Intensity</p>
+            </NavLink>
+
+
+            <NavLink 
+              to="/your-usage"
+              className={({ isActive }) =>
+                isActive
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
+              } 
+            >
+              <span className="material-symbols-outlined mr-1">electric_bolt</span>
+              <p className='m-2'>Your Usage</p>
+            </NavLink>
+            
+            <NavLink 
+              to="/your-scores"
+              className={({ isActive }) =>
+                isActive
+                ? "text-lg text-bg-outer bg-primaryGradient1 hover:bg-bg-main pl-2 rounded-lg flex items-center"
+                : "text-lg text-text-colour-secondary hover:text-text-colour-primary bg-transparent hover:bg-bg-main pl-2 rounded-lg flex items-center"
+              } 
+            >
+              <span className="material-symbols-outlined mr-1">speed</span>
+              <p className='m-2'>Your Scores</p>
+            </NavLink>
+          </nav>
       </aside>
     </>
   );
