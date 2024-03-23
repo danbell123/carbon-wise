099c8eb722c6fc5e9f3fc06b93ba63c9ba6b85be
danbell123 <d.bell13@newcastle.ac.uk>
2024-03-23 22:38:31 +0000

    updated tailwind breakpoints

--- tailwind.config.js
+++ tailwind.config.js
@@ -55,6 +55,14 @@ module.exports = {
       },
       // Any other extensions from Figma...
     },
+    screens: {
+      'sm': '768px', 
+      'md': '768px', 
+      'lg': '1024px', 
+      'xl': '1280px', 
+      '2xl': '1536px', 
+      '3xl': '1920px', 
+    },
   },
   corePlugins: {
     preflight: false,
