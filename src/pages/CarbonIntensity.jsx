70a40eb669656e450eff3d8e8f7c7f0dbb778785
danbell123 <d.bell13@newcastle.ac.uk>
2024-04-08 02:59:35 +0100

    carbon intensity page styled and working

--- src/pages/CarbonIntensity.jsx
+++ src/pages/CarbonIntensity.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import 'react-datepicker/dist/react-datepicker.css';
 import { DatePicker, InputGroup } from 'rsuite';
-import { UsageStatCard } from '../components/summaryWidgets/UsageStatCard';
 import CarbonIntensityChart from '../components/dataVis/carbonIntensityChart';
 import { fetchFutureCIData, fetchPastCIData } from '../services/getCarbonIntensity';
 import cleanCIData from '../utils/cleanCIData';
@@ -9,59 +8,76 @@ import carbonIntensityDescription from '../utils/carbonIntensityDescription';
 import BarLoader from '../components/loader/barLoader';
 import "rsuite/dist/rsuite.css";
 import CarbonIntensityLive from '../components/intensity/CarbonIntensityLive';
-
+import regions from '../data/regions.json';
+import { getAuth } from 'firebase/auth';
+import fetchUserData from '../services/getUserDetails'; 
 
 function CarbonIntensityPage() {
-    const [cleanedData, setCleanedData] = useState([]);
+    const [cleanedFutureData, setCleanedFutureData] = useState([]);
+    const [cleanedPastData, setPastCleanedData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
-    const [intensityInfo, setIntensityInfo] = useState({ level: '', description: '' });
-    const [latestIntensityValue, setLatestIntensityValue] = useState();
-    const [regionID, setRegionID] = useState(8);
-    const [startDate, setStartDate] = useState(new Date(new Date().setHours(new Date().getHours() - 12)));
-    const [endDate, setEndDate] = useState(new Date());
+    const [regionID, setRegionID] = useState(null);
+    const [futureStartDate, setFutureStartDate] = useState(new Date(new Date().setHours(new Date().getHours() - 12)));
+    const [futureEndDate, setFutureEndDate] = useState(new Date());
+    const [pastStartDate, setPastStartDate] = useState(null);
+    const [pastEndDate, setPastEndDate] = useState(null);
     const [activeTab, setActiveTab] = useState('Live Data');
+    const [regionName, setRegionName] = useState(''); 
 
     useEffect(() => {
-        const fetchIntensityData = async () => {
+        const fetchUserRegion = async () => {
+            const auth = getAuth();
+            const user = auth.currentUser;
+            if (!user) throw new Error('No authenticated user found.');
+
+            // Fetch user data to get the region ID
+            const userData = await fetchUserData(user.uid);
+            setRegionID(userData.regionID);
+
+            const userRegion = regions.find(region => region.id === Number(regionID));
+            if (userRegion) {
+            setRegionName(userRegion.name); // Step 3: Dynamically set the region name
+            }
+        };
+
+        const fetchFutureIntensityData = async () => {
             setIsLoading(true);
             try {
-                // Use startDate and endDate in the API calls
-                const pastIntensityData = await fetchPastCIData(startDate.toISOString(), endDate.toISOString(), regionID);
-                
-                // For future data, calculate the difference in hours between endDate and now
-                const hoursDiff = Math.floor((endDate - new Date()) / 36e5);
-                const futureIntensityData = hoursDiff > 0 ? await fetchFutureCIData(hoursDiff, regionID) : [];
-        
-                // Clean the fetched data and set it to state
-                const newCleanedData = cleanCIData(pastIntensityData, futureIntensityData);
-                
-                // Calculate the latest intensity from the newly cleaned data
-                const now = new Date();
-                const closestPastData = newCleanedData
-                    .filter(d => new Date(d.from) <= now)
-                    .reduce((prev, current) => {
-                        return (prev === null || new Date(current.from) > new Date(prev.from)) ? current : prev;
-                    }, null);
-
-                const currentIntensityValue = closestPastData ? closestPastData.forecast : 0;
-                setCleanedData(newCleanedData);
-                setLatestIntensityValue(currentIntensityValue);
-                setIntensityInfo(carbonIntensityDescription(currentIntensityValue));
+                const futureIntensityData = await fetchFutureCIData(48, regionID);
+                setCleanedFutureData(futureIntensityData);
 
             } catch (error) {
-                console.error('Failed to fetch carbon intensity data:', error);
+                console.error('Failed to fetch future carbon intensity data:', error);
             } finally {
                 setIsLoading(false);
             }
         };
-    
-        fetchIntensityData();   
-    }, [regionID, startDate, endDate]);
+
+        fetchUserRegion();
+
+        if (pastStartDate && pastEndDate) {
+            setIsLoading(true);
+            const fetchPastIntensityData = async () => {
+                try {
+                    const pastIntensityData = await fetchPastCIData(pastStartDate.toISOString(), pastEndDate.toISOString(), regionID);
+                    const newCleanedPastData = cleanCIData(pastIntensityData);
+                    setPastCleanedData(newCleanedPastData);
+                } catch (error) {
+                    console.error('Failed to fetch past carbon intensity data:', error);
+                } finally {
+                    setIsLoading(false);
+                }
+            };
+            fetchPastIntensityData();
+        }
+        
+        fetchFutureIntensityData();   
+    }, [regionID, futureStartDate, futureEndDate, pastStartDate, pastEndDate]); 
 
   return (
     <div className="p-5 pt-28 sm:pt-10">
         <h1 className="text-4xl w-full m-0 text-text-colour-primary">Carbon Intensity</h1>
-        <h3 className="m-0 text-lg font-normal text-text-colour-secondary">Monitoring your electricty usage 24/7</h3>
+        <p className="text-base pt-2 text-text-colour-secondary">Carbon intensity is a measure of the amount of carbon (CO2) emissions produced per unit of electricity consumed. The lower the carbon intensity, the cleaner the electricity. Aim to use your electricity during times of lower carbon intensity.</p>
 
         <div className="pt-6">
             <nav className="flex space-x-4" aria-label="Tabs">
@@ -83,62 +99,44 @@ function CarbonIntensityPage() {
             }
             {activeTab === 'Past Data' && 
                 <div>
-                    <div className="date-picker-container">
-                    <InputGroup style={{ width: "50%", border: "0px" }}>
-                        <DatePicker 
-                            format="dd-MM-yyyy - HH:mm"
-                            block appearance="subtle"
-                            style={{ width: "50%", marginRight: "10px" }} // Added marginRight
-                            selected={startDate}
-                            onChange={date => setStartDate(date)} 
-                            showTimeSelect
-                            dateFormat="Pp"
-                            timeIntervals={15}
-                        />
-                        <InputGroup.Addon style={{ margin: "0 10px" }}>to</InputGroup.Addon> {/* Adjusted spacing around Addon */}
-                        <DatePicker
-                            format="dd-MM-yyyy - HH:mm"
-                            block appearance="subtle"
-                            style={{ width: "50%", marginLeft: "10px" }} // Added marginLeft
-                            selected={endDate}
-                            onChange={date => setEndDate(date)} 
-                            showTimeSelect
-                            dateFormat="Pp"
-                            timeIntervals={15}
-                        />
-                    </InputGroup>
+                    <div className='flex flex-col gap-1 justify-start mb-4'>
+                        <h1 className="text-xl m-0 text-text-colour-primary font-semibold">Past Carbon Intensity Data</h1>
+                        <p className="text-base pt-2 text-text-colour-secondary">View past carbon intensity data for {regionName}</p>
                     </div>
-                    <CarbonIntensityChart data={cleanedData} />       
-                </div>}
+                    <div className="date-picker-container mb-4">
+                        <InputGroup style={{ width: "50%", border: "0px" }}>
+                            <DatePicker 
+                                format="dd-MM-yyyy - HH:mm"
+                                block appearance="subtle"
+                                style={{ width: "50%", marginRight: "10px" }} 
+                                selected={pastStartDate}
+                                onChange={date => setPastStartDate(date)} 
+                                showTimeSelect
+                                dateFormat="Pp"
+                            />
+                            <InputGroup.Addon style={{ margin: "0 10px" }}>to</InputGroup.Addon>
+                            <DatePicker
+                                format="dd-MM-yyyy - HH:mm"
+                                block appearance="subtle"
+                                style={{ width: "50%", marginLeft: "10px" }}
+                                selected={pastEndDate}
+                                onChange={date => setPastEndDate(date)} 
+                                showTimeSelect
+                                dateFormat="Pp"
+                            />
+                        </InputGroup>
+                    </div>
+                    <CarbonIntensityChart data={cleanedPastData} />       
+                </div>
+            }
             {activeTab === 'Future Data' && 
             <div>
-                <div className="date-picker-container">
-                <InputGroup style={{ width: "50%", border: "0px" }}>
-                    <DatePicker 
-                        format="dd-MM-yyyy - HH:mm"
-                        block appearance="subtle"
-                        style={{ width: "50%", marginRight: "10px" }} // Added marginRight
-                        selected={startDate}
-                        onChange={date => setStartDate(date)} 
-                        showTimeSelect
-                        dateFormat="Pp"
-                        timeIntervals={15}
-                    />
-                    <InputGroup.Addon style={{ margin: "0 10px" }}>to</InputGroup.Addon> {/* Adjusted spacing around Addon */}
-                    <DatePicker
-                        format="dd-MM-yyyy - HH:mm"
-                        block appearance="subtle"
-                        style={{ width: "50%", marginLeft: "10px" }} // Added marginLeft
-                        selected={endDate}
-                        onChange={date => setEndDate(date)} 
-                        showTimeSelect
-                        dateFormat="Pp"
-                        timeIntervals={15}
-                    />
-                </InputGroup>
+                <div className='flex flex-col gap-1 justify-start mb-8'>
+                    <h1 className="text-xl m-0 text-text-colour-primary font-semibold">{regionName}'s' 48 Hour Forecast</h1>
+                    <p className="text-base pt-2 text-text-colour-secondary">To keep your carbon footprint low, try use your energy during low times.</p>
                 </div>
-                    <CarbonIntensityChart data={cleanedData} />       
-                </div>}
+                <CarbonIntensityChart data={cleanedFutureData} />       
+            </div>}
         </div>
     </div>
   );
