d06b4ab1efe2567ab2427d4fede41300e0919049
danbell123 <d.bell13@newcastle.ac.uk>
2024-04-10 14:40:50 +0100

    added glass effect to monthly calendar cards

--- src/components/scoresTabs/MonthlyScoreSummaryCard.jsx
+++ src/components/scoresTabs/MonthlyScoreSummaryCard.jsx
@@ -8,7 +8,7 @@ const DateSummary = ({ date, score }) => {
 
   // If there is a date, return the full content
   return (
-    <div className="h-min bg-bg-main justify-center items-center text-center flex flex-col gap-2 p-2 rounded-lg">
+    <div className="h-min bg-bg-main-transparent glassEffect justify-center items-center text-center flex flex-col gap-2 p-2 rounded-lg">
         <p className="text-text-colour-secondary m-0 text-base">{date && new Date(date).getDate()}</p>
         <h2 className="veryhigh-score-text m-0 text-3xl">{score !== null ? score : "-"}</h2>
         <p className='text-text-colour-tertiary m-0 text-xs'>/100</p>
