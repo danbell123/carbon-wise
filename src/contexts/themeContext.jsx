9486d87109db0768570826bbb2b033bf74fd0cc8
danbell123 <d.bell13@newcastle.ac.uk>
2024-04-28 21:13:54 +0100

    dark theme default

--- src/contexts/themeContext.jsx
+++ src/contexts/themeContext.jsx
@@ -6,7 +6,7 @@ const ThemeContext = createContext();
 export const useTheme = () => useContext(ThemeContext);
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'light');
+  const [theme, setTheme] = useState(localStorage.getItem('theme') || 'dark');
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme);
