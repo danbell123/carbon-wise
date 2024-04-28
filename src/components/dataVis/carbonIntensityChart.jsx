022680aa21deb24c6411e9623354b1379d6239fe
danbell123 <d.bell13@newcastle.ac.uk>
2024-04-28 21:58:00 +0100

    added midnight reference lines for better accessability

--- src/components/dataVis/carbonIntensityChart.jsx
+++ src/components/dataVis/carbonIntensityChart.jsx
@@ -1,25 +1,35 @@
 import React from 'react';
 import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, Tooltip, ReferenceLine } from 'recharts';
-import { parseISO, format } from 'date-fns';
+import { parseISO, format, startOfDay, isSameDay } from 'date-fns';
 import { useViewportWidth } from '../../hooks/useViewportWidth';
 import carbonIntensityDescription from '../../utils/carbonIntensityDescription'; 
 
 const CarbonIntensityChart = ({ data }) => {
-    // Immediately use the hook at the top level
     const width = useViewportWidth();
 
-    // Check for data validity at the top level before any other logic
     if (!data || data.length === 0) {
         return <p>No data available.</p>;
     }
 
-    // Date processing
+    const getMidnights = (data) => {
+        const midnights = [];
+        data.forEach(item => {
+            const date = parseISO(item.from);
+            const startOfDate = startOfDay(date);
+            if (!midnights.some(d => isSameDay(d, startOfDate))) {
+                midnights.push(startOfDate);
+            }
+        });
+        return midnights;
+    };
+
+    const midnights = getMidnights(data);
+
     const now = new Date();
     const minutes = now.getMinutes();
     now.setMinutes(minutes < 30 ? 0 : 30, 0);
     const nowForReferenceLine = format(now, "yyyy-MM-dd'T'HH:mm'Z'");
 
-    // Determine viewport-based styling
     let interval, fontSize;
     if (width < 768) {
         interval = 14;
@@ -42,45 +52,7 @@ const CarbonIntensityChart = ({ data }) => {
             return format(parseISO(tickItem), 'HH:mm');
         } catch (error) {
             console.error("Error parsing date for X axis:", error);
-            return tickItem; // Fallback to showing unformatted date
-        }
-    };
-
-    const CustomTooltip = ({ active, payload }) => {
-        if (active && payload && payload.length) {
-            const { forecast, from } = payload[0].payload;    
-            return (
-                <div className="custom-tooltip bg-bg-outer rounded-lg p-2 flex flex-col gap-2">
-                    <div className='flex flex-row justify-start gap-5'>
-                        <span className="material-symbols-outlined text-text-colour-primary">schedule</span>
-                        <p className="text-text-colour-primary">{format(parseISO(from), 'dd/MM HH:mm')}</p>
-                    </div>
-                    <div className='flex flex-row justify-start gap-5'>
-                        <span className="material-symbols-outlined text-text-colour-primary">scatter_plot</span>
-                        <p className="text-text-colour-primary">{`${forecast} gCO2/kWh`}</p>
-                    </div>
-                </div>
-            );
-        }
-    
-        return null;
-    };
-
-    const CustomizedDot = (props) => {
-        const { cx, cy, payload } = props;
-        try {
-            const intensityLevel = carbonIntensityDescription(payload.forecast).level;
-            const colorMap = {
-                'Very Low': 'var(--veryLowColour)',
-                'Low': 'var(--lowColour)',
-                'Moderate': 'var(--midColour)',
-                'High': 'var(--highColour)',
-                'Very High': 'var(--veryHighColour)'
-            };
-            return <circle cx={cx} cy={cy} r={4} fill={colorMap[intensityLevel] || 'gray'} />;
-        } catch (error) {
-            console.error("Error rendering custom dot:", error);
-            return null; // Avoid rendering a faulty dot
+            return tickItem; // Fallback
         }
     };
 
@@ -99,13 +71,50 @@ const CarbonIntensityChart = ({ data }) => {
                         tick={axisTickStyle}
                     />
                     <YAxis stroke="var(--text-colour-secondary)" tick={axisTickStyle} />
-                    <Tooltip content={<CustomTooltip />} />
+                    <Tooltip content={({ active, payload }) => {
+                        if (active && payload && payload.length) {
+                            const { forecast, from } = payload[0].payload;    
+                            return (
+                                <div className="custom-tooltip bg-bg-outer rounded-lg p-2 flex flex-col gap-2">
+                                    <div className='flex flex-row justify-start gap-5'>
+                                        <span className="material-symbols-outlined text-text-colour-primary">schedule</span>
+                                        <p className="text-text-colour-primary">{format(parseISO(from), 'dd/MM HH:mm')}</p>
+                                    </div>
+                                    <div className='flex flex-row justify-start gap-5'>
+                                        <span className="material-symbols-outlined text-text-colour-primary">scatter_plot</span>
+                                        <p className="text-text-colour-primary">{`${forecast} gCO2/kWh`}</p>
+                                    </div>
+                                </div>
+                            );
+                        }
+                        return null;
+                    }} />
                     <Line
                         type="linear"
                         dataKey="forecast"
                         stroke="var(--text-colour-secondary)"
-                        dot={<CustomizedDot />}
+                        dot={({ cx, cy, payload }) => {
+                            const { forecast } = payload;
+                            const intensityLevel = carbonIntensityDescription(forecast).level;
+                            const colorMap = {
+                                'Very Low': 'var(--veryLowColour)',
+                                'Low': 'var(--lowColour)',
+                                'Moderate': 'var(--midColour)',
+                                'High': 'var(--highColour)',
+                                'Very High': 'var(--veryHighColour)'
+                            };
+                            return <circle cx={cx} cy={cy} r={4} fill={colorMap[intensityLevel] || 'gray'} />;
+                        }}
                     />
+                    {midnights.map(date => (
+                        <ReferenceLine
+                            key={+date}
+                            x={format(date, "yyyy-MM-dd'T'HH:mm'Z'")}
+                            stroke="var(--text-colour-secondary)"
+                            strokeDasharray="5, 5"
+                            label={{ value: `${format(date, "dd/MM")}`, position: 'insideTop', fill: 'var(--text-colour-primary)' }}
+                        />
+                    ))}
                     <ReferenceLine
                         x={nowForReferenceLine}
                         stroke="var(--text-colour-secondary)"
