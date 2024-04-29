3aff3444be9c36d8dfc2e8d6dce3b2ec730a0fe9
danbell123 <d.bell13@newcastle.ac.uk>
2024-04-29 01:22:28 +0100

    fix to icon rotation on forecast cards

--- src/components/summaryWidgets/peakTimeCard.jsx
+++ src/components/summaryWidgets/peakTimeCard.jsx
@@ -20,10 +20,16 @@ const PeakTimeCard = ({ period, forecast, date }) => {
         ? "flex flex-row glassEffect bg-greenGradient p-5 mb-4 rounded-xl items-left justify-between gap-6"
         : "flex flex-row glassEffect bg-redGradient p-5 mb-4 rounded-xl items-left justify-between gap-6";
 
+    const iconClasses = isLowCarbon
+        ? "material-symbols-outlined text-text-colour-primary mt-1 text-3xl"
+        : "material-symbols-outlined text-text-colour-primary mt-1 text-3xl rotate-180";
+
     return (
         <div className={cardClasses}>
             <div className='flex flex-row gap-3 '>
-                <span className="material-symbols-outlined text-text-colour-primary mt-1 text-3xl" style={{ fontSize: '30px' }}>arrow_cool_down</span>
+                <div>
+                    <span className={iconClasses} style={{ fontSize: '30px' }}>arrow_cool_down</span>
+                </div>
                 <div className='flex flex-col'>
                     <h2 className='text-text-colour-primary m-0 text-xl pb-4'>{intensityInfo.level} Carbon</h2>
                     <p className='text-text-colour-primary text-base m-0'>{forecast} gCO2/kWh</p>
